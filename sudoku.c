ff51db8fd6fce05d86e3df6bd1d8ab1c7fdb2ea6
Your Name <you@example.com>
2024-05-16 21:51:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -119,6 +119,9 @@ Node* DFS(Node* initial, int* cont){
    while(get_size(S) != 0){
       Node* n = top(S);
       pop(S);
+      if(is_final(n)==1){
+         return n;
+      }
    }
    
   return NULL;
